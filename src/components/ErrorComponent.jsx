07c62fa17dc567cd13ee7fc0ed8badd76aa8fc23
Kam Cheng <kamcheng@hotmail.co.uk>
2022-03-10 16:42:34 +0000

    impletemented error handling in delete comment

--- src/components/ErrorComponent.jsx
+++ src/components/ErrorComponent.jsx
@@ -1,7 +1,11 @@
 export default function ErrorComponent({ error: { status, message } }) {
-  return (
-    <p className="error-message">
-      Error: {status} {message}
-    </p>
-  );
+  if (message) {
+    return (
+      <p className="error-message">
+        Error: {status} {message}
+      </p>
+    );
+  } else {
+    return null;
+  }
 }
