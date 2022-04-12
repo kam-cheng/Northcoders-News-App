6c61c38aea8143dbcb3a31010bac3f7ce6718329
kam-cheng <kamcheng@hotmail.co.uk>
2022-04-12 20:35:51 +0100

    updated order button with material UI Button Group

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,33 +1,96 @@
-import DropDown from "./DropDown";
-import { Link } from "react-router-dom";
+import { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import Grow from "@mui/material/Grow";
+import Paper from "@mui/material/Paper";
+import Popper from "@mui/material/Popper";
+import MenuItem from "@mui/material/MenuItem";
+import MenuList from "@mui/material/MenuList";
 
 export default function Order({ order, setOrder }) {
+  const navigate = useNavigate();
   const orderArray = [
     { name: "ascending", apiValue: "asc" },
     { name: "descending", apiValue: "desc" },
   ];
 
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
+  const [selectedIndex, setSelectedIndex] = useState(1);
+
+  const handleMenuItemClick = (event, index) => {
+    setSelectedIndex(index);
+    setOpen(false);
+  };
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
     <>
-      <h4>
-        <label htmlFor="order-dropdown">order:</label>
-        <DropDown list={order} name={"order"}>
-          {orderArray.map((element) => {
-            return (
-              <Link
-                to={`?order:${element.name}`}
-                key={element.name}
-                className="dropdown-link"
-                onClick={() => {
-                  setOrder(element);
-                }}
-              >
-                {element.name}
-              </Link>
-            );
-          })}
-        </DropDown>
-      </h4>
+      <ButtonGroup
+        variant="contained"
+        ref={anchorRef}
+        aria-label="split button"
+      >
+        <Button
+          endIcon={<ArrowDropDownIcon />}
+          aria-controls={open ? "split-button-menu" : undefined}
+          aria-expanded={open ? "true" : undefined}
+          aria-label="select merge strategy"
+          aria-haspopup="menu"
+          onClick={handleToggle}
+        >{`Sort By ${orderArray[selectedIndex].name}`}</Button>
+      </ButtonGroup>
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === "bottom" ? "center top" : "center bottom",
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList id="split-button-menu" autoFocusItem>
+                  {orderArray.map((option, index) => (
+                    <MenuItem
+                      key={option.name}
+                      selected={index === selectedIndex}
+                      onClick={(event) => {
+                        setOrder(option);
+                        navigate(`?order:${option.name}`);
+                        handleMenuItemClick(event, index);
+                      }}
+                    >
+                      {option.name}
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
     </>
   );
 }
