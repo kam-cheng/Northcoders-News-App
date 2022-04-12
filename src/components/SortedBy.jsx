83bbc2d0ebae5cade2ccf76242f6649b1d9834b8
kam-cheng <kamcheng@hotmail.co.uk>
2022-04-12 21:18:00 +0100

    removed redundant import

--- src/components/SortedBy.jsx
+++ src/components/SortedBy.jsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
