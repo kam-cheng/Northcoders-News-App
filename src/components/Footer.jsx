ae14b0422dc5523865634f1b2425280995adf688
kam-cheng <kamcheng@hotmail.co.uk>
2022-04-14 17:03:44 +0100

    added links to footer and restyled with materialUI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,40 @@
-import "./Footer.css";
+import { Typography, Box, Button } from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
 
 export default function Footer() {
   return (
     <footer>
-      <p>Author: Kam Cheng</p>
-      <a href="mailto:kamcheng@hotmail.co.uk">kamcheng@hotmail.co.uk</a>
+      <Box sx={{ backgroundColor: "primary.dark", color: "white", p: 5 }}>
+        <Typography variant="body1" m={1}>
+          Author: Kam Cheng
+        </Typography>
+        <Button
+          href="mailto:kamcheng@hotmail.co.uk"
+          startIcon={<EmailIcon fontSize="large" />}
+          size="large"
+          color="inherit"
+        >
+          Email
+        </Button>
+        <Button
+          href="https://www.linkedin.com/in/kam-cheng/"
+          startIcon={<LinkedInIcon fontSize="large" />}
+          size="large"
+          color="inherit"
+        >
+          LinkedIn
+        </Button>
+        <Button
+          href="https://github.com/kam-cheng/"
+          startIcon={<GitHubIcon fontSize="large" />}
+          size="large"
+          color="inherit"
+        >
+          Github
+        </Button>
+      </Box>
     </footer>
   );
 }
