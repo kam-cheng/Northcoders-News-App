ac8591dd8177dc80853b9e36d9969321fcc88064
kam-cheng <kamcheng@hotmail.co.uk>
2022-04-14 17:35:05 +0100

    updated error page component with Material UI

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
-import "./ErrorPage.css";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 export default function ErrorPage() {
   return (
     <>
-      <h3 className="errorpage-message">
-        Oops...looks like you are at the wrong page!
-      </h3>
-      <Link to="/">
-        <h3 className="errorpage-message">Go Back to HomePage</h3>
-      </Link>
+      <Box m={10}>
+        <Typography variant="h5" gutterBottom>
+          Oops...looks like you are at the wrong page!
+        </Typography>
+        <Button>
+          <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+            <Typography variant="h5">Go Back to HomePage</Typography>
+          </Link>
+        </Button>
+      </Box>
     </>
   );
 }
